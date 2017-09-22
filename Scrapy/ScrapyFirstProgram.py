c47b604a50687af5af3503603b32d099b14dcdbc
yanhao <2683751052@qq.com>
2017-09-22 17:30:48 +0800

    few update 8

---
+++ Scrapy/ScrapyFirstProgram.py
@@ -0,0 +1,7 @@
+# -*- coding:UTF-8 -*-
+#下面的代码是scrapy框架第一次使用，用来测试其功能！
+# Step 1: Build a Scrapy spider program ,now choose a file path, D://pycodes/
+# Step 2: generate a spider in out program
+# Step 3: config the spider which has been generated
+# Step 4: run spider and get the webpage
+
